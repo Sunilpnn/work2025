d63eb6acfaa0ee36482ea4d9a0fc987278a634b5
Sunil PN <sunildiroi@gmail.com>
2025-07-15 20:28:22 +0530

    finished the car project using array

--- car1/Car.java
+++ car1/Car.java
@@ -3,16 +3,85 @@ public class Car{
 String name;
 String color;
 
-Door door1;
-Door door2;
-Door door3;
-Door door4;
-
-Tyre tyre1;
-Tyre tyre2;
-Tyre tyre3;
-Tyre tyre4;
+Door[] d = new Door[4];
+Tyre[] t = new Tyre[4];
 
 Steering st;
+
+
+	public Car(){
+	
+    d[0]= new Door();
+	d[0].name= "fRight";
+	
+    d[1]= new Door();
+	d[1].name= "fLeft";
+	
+	d[2]= new Door();
+	d[2].name= "bLeft";
+	
+	d[3] = new Door();
+	d[3].name= "bRight";
+	
+	
+	
+	t[0] = new Tyre();
+	t[0].brand="MRF";
+	t[0].size=20;
+	
+	t[1] = new Tyre();
+	t[1].brand="MRF";
+	t[1].size=20;
+	
+	t[2] = new Tyre();
+	t[2].brand="MRF";
+	t[2].size=20;
+	
+	t[3] = new Tyre();
+	t[3].brand="MRF";
+	t[3].size=20;
+	
+	
+	
+	st = new Steering();
+    st.name="Power Steering";
+    st.size=15;	
+	
+	
+	name="BMW";
+	color="RED";
+	
+	}
+
+
+public void carDetails(){
+	
+	
+	System.out.println("Car name :"+name);
+	System.out.println("Car color:"+color);
+	
+	System.out.println("Door :"+d[0].name + "\n" + "Door :"+ d[1].name + "\n" + "Door :"+ d[2].name + "\n" +"Door :"+ d[3].name + "\n");
+     
+	System.out.println("Tyre 1 :"+t[0].brand);
+	System.out.println("Tyre 1:"+t[0].size);
+	
+	System.out.println("Tyre 2:"+t[1].brand);
+	System.out.println("Tyre 2:"+t[1].size);
+	
+	System.out.println("Tyre 3:"+t[2].brand);
+	System.out.println("Tyre 3:"+t[2].size);
+	
+	System.out.println("Tyre 4:"+t[3].brand);
+	System.out.println("Tyre 4:"+t[3].size);
+	
+	
+	System.out.println("Steering :"+st.name);
+	System.out.println("Steering :"+st.size);
+
+	
+	System.out.println("Car is assembled Successfully ");
+	
+	
+}
 
 }
--- car1/Tdd.java
+++ car1/Tdd.java
@@ -1,69 +1,16 @@
     public class Tdd {
 
     public static void main (String[]args){
+	 
 	
+	
+	 
 	Car car = new Car();
 	
+	car.carDetails();
+ 
 	
-	
-	car.door1 = new Door();
-	car.door1.name = "fRight";
-	
-	car.door2 = new Door();
-	car.door2.name = "fLeft";
-	car.door3 = new Door();
-	car.door3.name = "bRight";
-	car.door4 = new Door();
-	car.door4.name ="bleft";
-	
-	car.tyre1 = new Tyre();
-	car.tyre1.brand="MRF";
-	car.tyre1.size=20;
-	
-	car.tyre2 = new Tyre();
-	car.tyre2.brand="MRF";
-	car.tyre2.size=20;
-	
-	car.tyre3 = new Tyre();
-	car.tyre3.brand="MRF";
-	car.tyre3.size=20;
-	
-	car.tyre4 = new Tyre();
-	car.tyre4.brand="MRF";
-	car.tyre4.size=20;
-	
-	
-	car.st = new Steering();
-    car.st.name="Power Steering";
-    car.st.size=15;	
-	
-	
-	car.name="BMW";
-	car.color="RED";
-	
-	System.out.println("Car name :"+car.name);
-	System.out.println("Car color:"+car.color);
-	
-	System.out.println("Door :"+car.door1.name + "\n" + "Door :"+ car.door2.name + "\n" + "Door :"+ car.door3.name + "\n" +"Door :"+ car.door4.name + "\n");
-     
-	System.out.println("Tyre 1 :"+car.tyre1.brand);
-	System.out.println("Tyre 1:"+car.tyre1.size);
-	
-	System.out.println("Tyre 2:"+car.tyre2.brand);
-	System.out.println("Tyre 2:"+car.tyre2.size);
-	
-	System.out.println("Tyre 3:"+car.tyre3.brand);
-	System.out.println("Tyre 3:"+car.tyre3.size);
-	
-	System.out.println("Tyre 4:"+car.tyre4.brand);
-	System.out.println("Tyre 4:"+car.tyre4.size);
-	
-	
-	System.out.println("Steering :"+car.st.name);
-	System.out.println("Steering :"+car.st.size);
 
-	
-	System.out.println("Car is assembled Successfully ");
 	
 	}
     
